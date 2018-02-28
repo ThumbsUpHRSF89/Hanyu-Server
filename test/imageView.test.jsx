8c30b90a137579f6339d877d8e0822f6544d0bd2
ysera11 <hanyuzhu94@gmail.com>
2018-02-28 14:03:55 -0800

    fix path in test suits

--- test/imageView.test.jsx
+++ test/imageView.test.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ImageView from '../client/component/imageView';
-import ImageList from '../client/component/imageList';
-import MainImage from '../client/component/mainImage';
+import ImageView from '../client/components/imageView';
+import ImageList from '../client/components/imageList';
+import MainImage from '../client/components/mainImage';
 
 describe('<ImageView />', () => {
   it('should render an <ImageList /> component', () => {
-    const wrapper = shallow(<ImageView />);
+    const wrapper = shallow(<ImageView image={[]} />);
     expect(wrapper.find(ImageList).length).toEqual(1);
   });
 
   it('should render an <MainImage /> component', () => {
-    const wrapper = shallow(<ImageView />);
+    const wrapper = shallow(<ImageView image={[]} />);
     expect(wrapper.find(MainImage).length).toEqual(1);
   });
 });

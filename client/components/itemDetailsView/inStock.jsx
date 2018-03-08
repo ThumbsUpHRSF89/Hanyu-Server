858411cbb510dd49d86a4139738732c51fc813ee
ysera11 <hanyuzhu94@gmail.com>
2018-03-07 19:21:19 -0800

    implement inStock component

--- client/components/itemDetailsView/inStock.jsx
+++ client/components/itemDetailsView/inStock.jsx
@@ -3,11 +3,19 @@ import PropTypes from 'prop-types';
 
 const InStock = ({ inStock }) => (
   <div>
+    {inStock <= 5 &&
     <span className="size-medium color-price">
       {`Only ${inStock} left in stock - order soon.`}
-    </span>
-    <span></span>
+    </span>}
+    {inStock > 5 &&
+    <span className="size-medium color-success">
+      In Stock.
+    </span>}
   </div>
 );
+
+InStock.propTypes = {
+  inStock: PropTypes.number.isRequired,
+};
 
 export default InStock;

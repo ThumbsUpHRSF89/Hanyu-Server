a7bad3f5971b3d9ffeff71b562e3b0e3b88af344
ysera11 <hanyuzhu94@gmail.com>
2018-03-07 18:38:33 -0800

    implement price component

--- client/components/itemDetailsView/price.jsx
+++ client/components/itemDetailsView/price.jsx
@@ -2,9 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Price = ({ price, shipping }) => (
-  <div>
-    Price: {price}
+  <div id="price">
+    <table>
+      <tr>
+        <td className="size-base color-secondary">
+          Price:
+        </td>
+        <td>
+          <span className="size-medium color-price">{`$${price}`}</span>
+          <span className="size-base color-base">
+            &nbsp;&&nbsp;
+            <b>FREE Shipping</b>
+            {shipping !== 0 && ' on orders over $25.'}
+          </span>
+        </td>
+      </tr>
+    </table>
   </div>
 );
+
+Price.propTypes = {
+  price: PropTypes.number.isRequired,
+  shipping: PropTypes.number.isRequired,
+};
 
 export default Price;
